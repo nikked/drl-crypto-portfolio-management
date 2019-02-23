9bb80fc67150fda3691ba4941b231ab402b94afc
Nikke <nikolai@vainu.io>
2019-02-23 19:21:20 +0200

    fixed test_rl_algo pylint errors

--- src/test_rl_algorithm.py
+++ src/test_rl_algorithm.py
@@ -1,9 +1,9 @@
-from src.params import PF_INIT_TEST, LENGTH_TENSOR
-
 import numpy as np
 
+from src.params import PF_INIT_TEST, LENGTH_TENSOR
 
-def test_rl_algorithm(
+
+def test_rl_algorithm(  # pylint:  disable=too-many-arguments, too-many-locals
     actor,
     state_fu,
     done_fu,
@@ -19,15 +19,14 @@ def test_rl_algorithm(
     w_s,
     nb_stocks,
 ):
-    #######TEST#######
 
     w_init_test = np.array(np.array([1] + [0] * nb_stocks))
 
     # initialization of the environment
-    state, done = env.reset(w_init_test, PF_INIT_TEST, index=total_steps_train)
+    state, _ = env.reset(w_init_test, PF_INIT_TEST, index=total_steps_train)
 
-    state_eq, done_eq = env_eq.reset(w_eq, PF_INIT_TEST, index=total_steps_train)
-    state_s, done_s = env_s.reset(w_s, PF_INIT_TEST, index=total_steps_train)
+    state_eq, _ = env_eq.reset(w_eq, PF_INIT_TEST, index=total_steps_train)
+    state_s, _ = env_s.reset(w_s, PF_INIT_TEST, index=total_steps_train)
 
     for i in range(nb_stocks):
         state_fu[i], done_fu[i] = env_fu[i].reset(
@@ -51,21 +50,22 @@ def test_rl_algorithm(
         total_steps_train + total_steps_val - int(LENGTH_TENSOR / 2),
         total_steps_train + total_steps_val + total_steps_test - LENGTH_TENSOR,
     ):
-        X_t = state[0].reshape([-1] + list(state[0].shape))
-        W_previous = state[1].reshape([-1] + list(state[1].shape))
+        x_current = state[0].reshape([-1] + list(state[0].shape))
+        print(x_current)
+        w_previous = state[1].reshape([-1] + list(state[1].shape))
         pf_value_previous = state[2]
         # compute the action
-        action = actor.compute_W(X_t, W_previous)
+        action = actor.compute_W(x_current, w_previous)
         # step forward environment
-        state, reward, done = env.step(action)
-        state_eq, reward_eq, done_eq = env_eq.step(w_eq)
-        state_s, reward_s, done_s = env_s.step(w_s)
+        state, _, _ = env.step(action)
+        state_eq, _, _ = env_eq.step(w_eq)
+        state_s, _, _ = env_s.step(w_s)
 
         for i in range(nb_stocks):
             state_fu[i], _, done_fu[i] = env_fu[i].step(action_fu[i])
 
-        X_next = state[0]
-        W_t = state[1]
+        # x_next = state[0]
+        w_current = state[1]
         pf_value_t = state[2]
 
         pf_value_t_eq = state_eq[2]
@@ -73,12 +73,12 @@ def test_rl_algorithm(
         for i in range(nb_stocks):
             pf_value_t_fu[i] = state_fu[i][2]
 
-        dailyReturn_t = X_next[-1, :, -1]
+        # dailyReturn_t = x_next[-1, :, -1]
         if k % 20 == 0:
             print("current portfolio value", round(pf_value_previous, 0))
-            print("weights", W_previous)
+            print("weights", w_previous)
         p_list.append(pf_value_t)
-        w_list.append(W_t)
+        w_list.append(w_current)
 
         p_list_eq.append(pf_value_t_eq)
         p_list_s.append(pf_value_t_s)
