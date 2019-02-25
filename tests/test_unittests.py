7f249b22d403d4629fb28d5a24c629e1957a6101
nikked <nide@cs.helsinki.fi>
2019-02-26 00:36:24 +0200

    added an unittest for cryptopipe

--- tests/test_unittests.py
+++ tests/test_unittests.py
@@ -9,20 +9,29 @@ class TestTrainTestAnalyse(unittest.TestCase):
             "crypto_data": False,
             "gpu_device": None,
             "interactive_session": False,
-            "max_no_of_training_periods": 10000,
+            "max_no_of_training_periods": 1000,
             "no_of_assets": 2,
             "plot_results": False,
             "verbose": False,
             "n_batches": 1,
             "n_episodes": 1,
-            "window_length": 400,
-            "batch_size": 50,
+            "window_length": 300,
+            "batch_size": 10,
             "portfolio_value": 10000,
         }
 
-    def test_train_completes_fully(self):
+    def test_stock_train_completes_fully(self):
 
         train_test_analyse_rl_algorithm.main(**self.cli_options)
+
+        self.assertEqual(True, True)
+
+    def test_crypto_train_completes_fully(self):
+
+        crypto_options = self.cli_options
+        crypto_options["crypto_data"] = True
+
+        train_test_analyse_rl_algorithm.main(**crypto_options)
 
         self.assertEqual(True, True)
 
