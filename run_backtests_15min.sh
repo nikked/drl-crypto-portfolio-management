49ae69169ec09f7f8b9870bf9167ba2b3f5e2fc3
nikked <nide@cs.helsinki.fi>
2019-04-26 22:54:57 +0300

    adding bashscripts for different trade periods

---
+++ run_backtests_15min.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+while true; 
+    do 
+        python deep_rl_portfolio.py --calm_before_the_storm --trading_period_length 15min
+        python deep_rl_portfolio.py --awakening --trading_period_length 15min
+        python deep_rl_portfolio.py --ripple_bullrun --trading_period_length 15min
+        python deep_rl_portfolio.py --ethereum_valley --trading_period_length 15min
+        python deep_rl_portfolio.py --all-time_high --trading_period_length 15min
+        python deep_rl_portfolio.py --rock_bottom --trading_period_length 15min
+        python deep_rl_portfolio.py --recent --trading_period_length 15min
+    done
+
