cd20f7bd33c9e028978a095073dbc4689915e359
nikked <nide@cs.helsinki.fi>
2019-04-28 20:33:53 +0300

    adding first backtests again due to idioticy

--- run_eth.sh
+++ run_eth.sh
@@ -2,8 +2,8 @@
 
 while true;
     do
-        python deep_rl_portfolio.py --ethereum_valley --trading_period_length 30min
         python deep_rl_portfolio.py --ethereum_valley --trading_period_length 15min
         python deep_rl_portfolio.py --ethereum_valley --trading_period_length 5min
+        python deep_rl_portfolio.py --ethereum_valley --trading_period_length 30min
     done
 
