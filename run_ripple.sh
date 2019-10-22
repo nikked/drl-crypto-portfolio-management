0c142f5bb4d573c651e4e9c4acc9a6c9269ea785
Nikolai Linnansalo <nikolai@vainu.io>
2019-10-22 17:18:20 +0200

    removed old files

--- run_ripple.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-while true;
-    do
-        python deep_rl_portfolio.py --ripple_bull_run --trading_period_length 15min
-        python deep_rl_portfolio.py --ripple_bull_run --trading_period_length 5min
-        python deep_rl_portfolio.py --ripple_bull_run --trading_period_length 30min
-    done
-
